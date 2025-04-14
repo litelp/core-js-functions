9cf0eaa6e4354aa6bfa0ba93cd49cb088f70a1e1
litelp <lponomareva97@yandex.ru>
2025-04-14 15:19:16 +0400

    feat: add solution for task logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -181,8 +181,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function fn(...args) {
+    const { name } = func;
+    const str = args.map((item) => JSON.stringify(item)).join(',');
+    logFunc(`${name}(${str}) starts`);
+    const result = func(...args);
+    logFunc(`${name}(${str}) ends`);
+    return result;
+  };
 }
 
 /**
